58ff3cbbd738dbcf15e787a0c4241a87f4d793e3
jotadegc <josedavidgome96@gmail.com>
2021-04-27 10:47:23 +0200

    Version saludo

--- src/com/everis/bootcamp/GitBootcamp.java
+++ src/com/everis/bootcamp/GitBootcamp.java
@@ -1,5 +1,10 @@
 package com.everis.bootcamp;
 
 public class GitBootcamp {
-
+	
+	public static void main(){
+		
+		System.out.println("Hola mundo");
+		System.out.println("Hola bootcamp");
+		}
 }
